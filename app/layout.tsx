8ee01dafb185cd77831110a9ca47fddd8e7a62c5
55555-Jyeon <134191817+55555-Jyeon@users.noreply.github.com>
2024-02-25 22:06:13 +0900

    before deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+/* 
+font 최적화하기
+
+<body> className에 추가, 해당 글꼴이 애플리케이션 전체에 적용
+antialiased : 글꼴을 부드럽게 만드는 클래스
+*/
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
